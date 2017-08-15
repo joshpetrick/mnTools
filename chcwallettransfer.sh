8f2e6f57819805ffe2d05a6eb277d769f6909068
joshpetrick <petrick.josh@gmail.com>
2017-08-14 20:32:30 -0400

    template for chcwallettransfer

---
+++ chcwallettransfer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##########################################
+# Send Generated ChainCoins to your Wallet
+##########################################
+
+#What wallet address would you like to send to? This is mine as an example. Be sure to change it!
+wallet=changemetoyourlocaladdress
+
+#How much CHC did your Masternode Generate since last run?
+GeneratedCHC=$(/usr/local/bin/chaincoind getinfo | awk '/"balance" :/ { print $3 }' | awk -F',' ' { print $1"-"1001.0001000 }' | bc)
+
+if [[ $GeneratedCHC == -* ]]; then
+    exit 1
+   else
+    usr/local/bin/chaincoind sendtoaddress $wallet $GeneratedCHC
+    exit 1
+fi
