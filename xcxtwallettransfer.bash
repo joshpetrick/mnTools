4b092e8f6181576024cb68729449c6d9e399131b
joshpetrick <petrick.josh@gmail.com>
2017-08-31 21:25:56 -0400

    created xcxt transfer
    making auto transfer so i dont miss on staking :)

---
+++ xcxtwallettransfer.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##########################################
+# Send Generated Coinonatx to your Wallet
+##########################################
+
+#What wallet address would you like to send to? This is mine as an example. Be sure to change it!
+wallet=changemetoyourlocaladdress
+
+#How much xcxt did your Masternode Generate since last run?
+Generatedxcxt=$(/usr/local/bin/coinonatxd getinfo | awk '/"balance" :/ { print $3 }' | awk -F',' ' { print $1"-"5001.0001000 }' | bc)
+
+if [[ $Generatedxcxt == -* ]]; then
+    exit 1
+   else
+    /usr/local/bin/chaincoind sendtoaddress $wallet $Generatedxcxt
+    exit 1
+fi
