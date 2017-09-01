93370f9818bf16abfd386215b89dfeb1352bd2cd
joshpetrick <petrick.josh@gmail.com>
2017-08-31 22:08:30 -0400

    initial commi
    dynamic wallet backupper

---
+++ backupwallet.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+##########################################
+# Send Generated ChainCoins to your Wallet
+##########################################
+
+# Name of the binary to use commands on ex: PieCoind
+binary="/usr/local/bin/"$1
+backuplocation=$2
+filename=$(date '+%d-%m-%y').wallet.dat
+
+
+
+if [[ $# -gt 0 ]]
+then
+        if [ -f "$binary" ]
+        then
+                echo "$binary does zist"
+                if [ -f "$backuplocation" ]
+                then
+                        echo "$backuplocation zists"
+                else
+                        echo "Using current location"
+                fi
+        else
+                echo "$binary doesnt zist"
+        fi
+else
+	echo "In order for this script to be dynamic you MUST enter a binary.. an example for Piecoin would be:"
+	echo "backupwallet.sh PieCoind ~/Backups/"
+	
+fi
+
+
+#What wallet address would you like to send to? This is mine as an example. Be sure to change it!
+#walletaddress=$1
+
+#How much CHC did your Masternode Generate since last run?
+#Test=$(/usr/local/bin/PieCoind backupwallet $walletaddress)
+
+#echo "You chose: $walletaddress"
