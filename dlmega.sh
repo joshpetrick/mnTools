1c1e3a3cd0fa2ea3c3b739def999178b62dbf0f0
joshpetrick <petrick.josh@gmail.com>
2017-09-11 13:26:45 -0400

    tool to dl mega tools
    will use to upload daily bootstrap? cross fingers

---
+++ dlmega.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+megaToolsVersion="1.9.96" 
+megaTools="/home/$USER/megatools-$megaToolsVersion" 
+if [ ! -d $megaTools ]; 
+then 	
+sudo apt-get -y 
+install build-essential 
+libglib2.0-dev libssl-dev 
+libcurl4-openssl-dev libgirepository1.0-dev -y --force-yes --quiet 	
+cd /home/$USER 	
+wget "https://megatools.megous.com/builds/megatools-$megaToolsVersion.tar.gz" 	
+tar -xvf "./megatools-$megaToolsVersion.tar.gz" 	
+cd $megaTools 	
+./configure 	
+make 	
+cd .. 	
+rm -rf "./megatools-$megaToolsVersion.tar.gz" 
+fi
+
